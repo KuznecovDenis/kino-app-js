bd1b3f475bc1b869f5c68b2aa78bcae3c2e92d22
KuznecovDenis <kyznecov995@icloud.com>
2022-12-12 03:49:01 +0400

    add theme-light style and change some bugs

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ function changeTheme (btnSelector, themeClass) {
 
   if (localStorage.getItem('style-theme') == themeClass) {
     document.body.classList.toggle(themeClass)
-    themeBtn.classList.remove('active') // исправить стили и кнопку
-    // themeBtn.classList.add('active')
+    themeBtn.classList.add('active') 
   }
   
   themeBtn.addEventListener('click', () => {
@@ -14,12 +13,10 @@ function changeTheme (btnSelector, themeClass) {
   
     if (document.body.classList.contains(themeClass)) {
       localStorage.setItem('style-theme', themeClass)
-      themeBtn.classList.remove('active') // исправить стили и кнопку
-      // themeBtn.classList.add('active')
+      themeBtn.classList.add('active') 
     } else {
-      localStorage.setItem('style-theme', '');
-      themeBtn.classList.add('active') // исправить стили и кнопку
-      // themeBtn.classList.remove('activ')
+      localStorage.setItem('style-theme', '')
+      themeBtn.classList.remove('active')
     }
   })
 }
