159b453b681d0a94841155ae86fe5e975929d20d
KuznecovDenis <kyznecov995@icloud.com>
2022-12-13 16:58:34 +0400

    add search movies logic

--- js/script.js
+++ js/script.js
@@ -27,6 +27,16 @@ changeTheme('.theme-btn', 'theme_light')
 function films() {
   const API_KEY = '9a8641e9-fa5d-4217-a734-9ef717964e5f'
   const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1'
+  const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
+  
+
+  // обернуть запросы в try/cath для отлова ошибок
+
+  // написать разметку и стили для модалки
+  // реализовать закрытие модалки по клику оверлею и ескейпу
+  // отправить запрос по айди фильма и вывести данные в модалку 
+
+  // поправить верстку карточек
 
 
   getMovies(API_URL_POPULAR)
@@ -44,11 +54,13 @@ function films() {
     const responseData = await response.json()
     console.log(responseData);
     showMoviesInPage(responseData)
-
   }
 
   function showMoviesInPage(data) {
     const movieContent = document.querySelector('.movieContent')
+
+    // очищает выдачу перед новыми результатами
+    movieContent.innerHTML = ''
 
     data.films.forEach(movie => {
       const movieEl = document.createElement('div')
@@ -65,15 +77,49 @@ function films() {
           <p class="movie-genre">${
             movie.genres.map(el => el.genre)
           }</p>
-          <p class="movie-reiting movie-reiting_yellow">${
-            9
-          }</p>
+          ${
+            (movie.rating && movie.rating !== "null") 
+              ? `<p class="movie-reiting movie-reiting_${getClassByRate(parseNum(movie.rating))}"> ${parseNum(movie.rating)} </p> `
+              : ''
+          }
         </div>
       `
       movieContent.appendChild(movieEl)
+      
     })
 
   }
+
+  function getClassByRate(rating) {
+    if (rating >= 7) {
+      return 'green'
+    } else if (rating >= 5) {
+      return 'yellow'
+    } else {
+      return 'red'
+    }
+  }
+
+  function parseNum(el) {
+    if (isFinite(el) && el <= 10 && el != NaN) {
+      return el
+    } else {
+      //обрабатываем проценты и возвращаем в виде обычного рейтинга
+      return (parseInt(el) / 100 * 10).toFixed(1)
+    }
+  }
+
+  const searchForm = document.querySelector('.search-area')
+  const searchInput = document.querySelector('.search-input')
+
+  searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const apiSearchUrl = `${API_URL_SEARCH}${searchInput.value}`
+
+    getMovies(apiSearchUrl)
+    searchInput.value = ''
+  })
 
 }
 
